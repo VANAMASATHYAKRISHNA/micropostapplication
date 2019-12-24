b5a97778f2c12ff6bf80fd8970fdde5efeeb57fc
Sathya Krishna <Sathya.krishna@solixcorp.com>
2019-12-24 11:28:49 +0530

    2nd commit

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,46 +1,41 @@
-const express=require('express');
-const mongodb=require('mongodb');
-const router=express.Router();
+const express = require('express');
+const mongodb = require('mongodb');
+const router = express.Router();
 //Get posts
-router.get('/',async(req,res)=>
-{
+router.get('/', async (req, res) => {
     //res.send("HEEE");
-    try{
-    const post=await loadpostcollection();
-  //  res.send("CONNECTED");
-    res.send(await post.find().toArray()); 
-    }
-    catch(err)
-    {
+    try {
+        const post = await loadpostcollection();
+        //  res.send("CONNECTED");
+        res.send(await post.find().toArray());
+    } catch (err) {
         res.status(401).json(err);
     }
 })
-router.post('/',async (req,res)=>{
-    const posts=await loadpostcollection();
-   const ans= await posts.insertOne({
-        text:req.body.text,
-        createdAt:new Date()
-        });
+router.post('/', async (req, res) => {
+    const posts = await loadpostcollection();
+    const ans = await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
     res.status(201).send(ans);
-}
-);
-router.delete('/:id',async(req,res)=>{
-    try{
-    const posts=await loadpostcollection();
-    const ans=await posts.deleteOne({_id:new mongodb.ObjectID(req.params.id)});
-     res.send(ans);
-    }
-    catch(err)
-    {
+});
+router.delete('/:id', async (req, res) => {
+    try {
+        const posts = await loadpostcollection();
+        const ans = await posts.deleteOne({
+            _id: new mongodb.ObjectID(req.params.id)
+        });
+        res.send(ans);
+    } catch (err) {
         res.send(err);
     }
 
 })
 
-async function loadpostcollection()
-{
-    const client = await mongodb.MongoClient.connect('mongodb+srv://azam-ahmed:ifteqari880@cluster0-9affu.mongodb.net/test',{
-        useNewUrlParser:true
+async function loadpostcollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://sathya:sathya143cluster0-9affu.mongodb.net/test', {
+        useNewUrlParser: true
     });
     return client.db('vue_express').collection('posts');
 }
@@ -51,4 +46,4 @@ async function loadpostcollection()
 
 
 
-module.exports=router;
+module.exports = router;
